8090d04797760834988e8e012388f5162a677433
Soam Vasani <soamvasani@platform9.com>
2018-03-08 15:58:30 -0800

    bugfixes for build.sh

--- build.sh
+++ build.sh
@@ -4,14 +4,18 @@
 # to do this automatically with the github api!)
 VERSION=0.6.0
 
+pushd docs
+
 echo "Running hugo"
 hugo
+
+popd
 
 echo "Removing old site"
 rm -rf dist/$VERSION
 
-echo "Copying new site"
-mv public dist/$VERSION
+echo "Moving new site into dist"
+mv docs/public dist/$VERSION
 
 echo "Making latest symlink"
 cd dist
