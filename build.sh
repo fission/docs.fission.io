d2104f2104c542193aa9d3bb427dff6f82a9ca81
Ta-Ching Chen <contact@tachingchen.com>
2018-05-05 19:24:05 +0800

    Prevent build.sh from generating non-html content

--- build.sh
+++ build.sh
@@ -22,6 +22,5 @@ echo "# Generated from _redirects.template" > dist/_redirects
 cat _redirects.template | sed -e "s/VERSION/$VERSION/g" >> dist/_redirects
 
 echo "Making index.html"
-echo "# Generated from index.html.template" > dist/index.html
 cat index.html.template | sed -e "s/VERSION/$VERSION/g" >> dist/index.html
 
