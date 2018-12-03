f02787deaa575aa2849373f10546b3108fad4a6e
Vishal Biyani <vishal@infracloud.io>
2018-12-03 22:28:38 +0530

    [WIP] changes to test the redirects across versions

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-source version.sh
+VERSION=latest
 
 cd docs
 
@@ -17,10 +17,9 @@ rm -rf dist/$VERSION
 echo "Moving new site into dist"
 mv docs/public dist/$VERSION
 
-echo "Making _redirects"
-echo "# Generated from _redirects.template" > dist/_redirects
-cat _redirects.template | sed -e "s/VERSION/$VERSION/g" >> dist/_redirects
+#echo "Making _redirects"
+#echo "# Generated from _redirects.template" > dist/_redirects
+#cat _redirects.template | sed -e "s/VERSION/$VERSION/g" >> dist/_redirects
 
 echo "Making index.html"
 cat index.html.template | sed -e "s/VERSION/$VERSION/g" > dist/index.html
-
