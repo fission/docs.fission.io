01f3b4c47f06faef1c57a50c1abe893a142384db
Vishal Biyani <vishal@infracloud.io>
2018-12-20 15:48:20 +0530

    Test commit to check working of changes

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-source version.sh
-
 cd docs
 
 echo "Running hugo"
@@ -17,10 +15,10 @@ rm -rf dist/$VERSION
 echo "Moving new site into dist"
 mv docs/public dist/$VERSION
 
-echo "Making _redirects"
-echo "# Generated from _redirects.template" > dist/_redirects
-cat _redirects.template | sed -e "s/VERSION/$VERSION/g" >> dist/_redirects
+### Keepting this for future refence, not in use ATM
+#echo "Making _redirects"
+#echo "# Generated from _redirects.template" > dist/_redirects
+#cat _redirects.template | sed -e "s/VERSION/$VERSION/g" >> dist/_redirects
 
-echo "Making index.html"
-cat index.html.template | sed -e "s/VERSION/$VERSION/g" > dist/index.html
-
+#echo "Making index.html"
+#cat index.html.template | sed -e "s/VERSION/$VERSION/g" > dist/index.html
